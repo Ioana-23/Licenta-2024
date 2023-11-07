734d309ea3f3213c8d7086988a071cf259ddc3c7
Ioana-23 <baciuioana23@gmail.com>
2023-11-07 17:31:10 +0200

    Added the code for reading the images

---
+++ app/readImages.py
@@ -0,0 +1,17 @@
+import os
+import matplotlib.pyplot as plt
+import pandas as pd
+from duke_dbt_data import dcmread_image
+
+df = pd.read_csv("data/BCS-DBT file-paths-train-v2.csv")
+
+view_series = df.iloc[0]
+view = view_series["View"]
+# if you have image data saved in classic path folder structure, read the file path from "classic_path"
+# image_path = os.path.join("/data", view_series["classic_path"])
+image_path = os.path.join("D:/Licenta/Proiect/saveData/trainData\manifest-1617905855234/", view_series["descriptive_path"])
+
+image = dcmread_image(fp=image_path, view=view)
+
+plt.imshow(image[0], cmap=plt.cm.gray)
+plt.show()
