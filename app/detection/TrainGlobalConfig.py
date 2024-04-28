589fb11a903120e4c19440c9fd446c0351b76be7
Ioana-23 <baciuioana23@gmail.com>
2024-04-28 13:02:46 +0300

    completed detection model

---
+++ app/detection/TrainGlobalConfig.py
@@ -0,0 +1,11 @@
+class TrainGlobalConfig:
+    num_workers = 0
+    batch_size = 16
+    n_epochs = 10
+    lr = 0.0002
+
+    folder = 'detection_checkpoints'
+
+    step_scheduler = False
+    validation_scheduler = True
+
